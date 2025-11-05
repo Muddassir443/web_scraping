3798951cd79ae6e8fc55da522b03ac858e8d0eee
Muddassir443 <165454867+Muddassir443@users.noreply.github.com>
2025-11-05 09:38:00 +0530

    Update full_project.py

--- full_project.py
+++ full_project.py
@@ -14,7 +14,7 @@ usr_name = drivers.find_element(By.ID, value="session_key")
 usr_name.send_keys("shaikmudassir443@gmail.com")
 
 pwd = drivers.find_element(By.ID, value="session_password")
-pwd.send_keys("$H@ik_443")
+pwd.send_keys("NOT_@_a_PASSWORD")
 
 sign_in = drivers.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 time.sleep(15)
@@ -22,6 +22,7 @@ time.sleep(15)
 show_all_jobs = drivers.find_element(By.LINK_TEXT, value="Show all").click()
 
 easy_apply = drivers.find_element(By.PARTIAL_LINK_TEXT, "Easy Apply").click()
+
 
 
 
